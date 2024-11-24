5c02c87ef8334c18fc8140db335d0f24e0647d07
arhyel24 <arhyelphilip024@gmail.com>
2024-11-24 03:26:35 +0100

    fire IJN

--- app/api/getcourses/route.ts
+++ app/api/getcourses/route.ts
@@ -7,10 +7,12 @@ export async function GET() {
   try {
     const courses = await Course.find({}).populate("chapters").lean();
 
+    console.log(courses);
+
     // Return the users with a 200 status code
     return NextResponse.json({ courses }, { status: 200 });
   } catch (error) {
-    console.error("Error fetching courses:", error);
+    console.error("Error fetching courses in json:", error);
     return NextResponse.json(
       { message: "Internal Server Error" },
       { status: 500 }
