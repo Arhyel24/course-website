13ec7ea5929fdd332bed57e70ca2526648859eab
Enoch Philip Dibal <arhyelphilip024@gmail.com>
2024-11-28 14:49:05 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4728250862053645"
-     crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4728250862053645" crossorigin="anonymous"></script>
         <ThemeModeScript />
       </head>
       <Flowbite theme={{ theme: flowbiteTheme }}>
